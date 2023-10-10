493af37762c720400583d9b5e19b63e142b43eba
5alidev <khalidbenhamou.dev@outlook.com>
2023-10-10 13:45:57 -0700

    print _puthcar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
 #include "main.h"
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
-	printf("_putchar\n");
+	char n[] = "_putchar";
+	int sizeOfc = sizeof(n);
+	int i;
+
+	for (i = 0; i < sizeOfc; i++)
+	{
+		_putchar(n[i]);
+	}	
+	_putchar('\n');
 	return (0);
 }

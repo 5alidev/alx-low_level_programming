acef5b1d9e349f3b253c2d890ef74fdb610d659a
5alidev <khalidbenhamou.dev@outlook.com>
2023-10-10 14:58:20 -0700

    check if is a letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
+ * _isalpha - checks if a character
+ * is a letter, lowercase or uppercase
  *
  * @c: character to check.
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }

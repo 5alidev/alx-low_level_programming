493302cb882b3458cba27c54db8c229a701c8a9a
5alidev <khalidbenhamou.dev@outlook.com>
2023-10-16 14:32:59 -0700

    fix betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98.
- *
- * @n : parameter variable to change the value to 98. 
- *
+ * @n: parameter variable to change the value to 98.
  * Return: Nothing (void)
  */
 void reset_to_98(int *n)

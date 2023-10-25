965a7f04f4a482a13bf93990ba7211639b4b196a
5alidev <khalidbenhamou.dev@outlook.com>
2023-10-25 15:56:00 -0700

    add find_sqrt function to the main function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,21 @@
 #include "main.h"
 /**
+ * find_sqrt - helper function to find the sqrt
+ *
+ * @n: the original number for which the sqrt is sought
+ * @guess: current guess for the sqrt
+ *
+ * Return: sqrt of @n
+ */
+int find_sqrt(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+	return (find_sqrt(n, guess + 1));
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number.
  *
  * @n: nubmer to calculate the square for.

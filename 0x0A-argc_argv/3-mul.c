443efdb8ca4eea863fc0523f2eb56946fd869e1c
5alidev <khalidbenhamou.dev@outlook.com>
2023-10-27 15:49:28 -0700

    add return (1)

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,10 @@ int main(int argc, char *argv[])
 	int mul = 1;
 
 	if (argc < 2)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul);
 	return (0);

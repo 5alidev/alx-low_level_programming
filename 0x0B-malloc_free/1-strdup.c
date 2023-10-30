8a5d7d8894ac32a30b1a8d2e0b87a1694152e5e7
5alidev <khalidbenhamou.dev@outlook.com>
2023-10-30 14:17:19 -0700

    add parentheses to return value

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	int size;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 	for (size = 0; str[size] != '\0'; size++)
 		;
 	copy = malloc(sizeof(char) * size + 1);

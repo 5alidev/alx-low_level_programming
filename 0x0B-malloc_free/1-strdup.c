af158fe2c7b9afaf85ccd4cb23d819bafeb85bf7
5alidev <khalidbenhamou.dev@outlook.com>
2023-10-30 14:14:39 -0700

    return NULL if str is NULL

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,6 +17,8 @@ char *_strdup(char *str)
 	unsigned int i;
 	int size;
 
+	if (str == NULL)
+		return NULL;
 	for (size = 0; str[size] != '\0'; size++)
 		;
 	copy = malloc(sizeof(char) * size + 1);

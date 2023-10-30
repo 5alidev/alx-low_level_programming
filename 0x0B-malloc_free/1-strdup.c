93d4dbd4cd00fe37df83e27d2e4eb157a06e28fb
5alidev <khalidbenhamou.dev@outlook.com>
2023-10-30 13:21:06 -0700

    fn returns pointer to new allocated memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - Returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string
+ * given as a parameter.
+ *
+ * @str: the string to make copy of.
+ *
+ * Return: Pointer to new allocated memory.
+ */
+char *_strdup(char *str)
+{
+	char *copy = NULL;
+
+	int i, strLength = strlen(str);
+
+	copy = malloc(sizeof(char) * strLength);
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < strLength; i++)
+	{
+		copy[i] = str[i];
+	}
+	return (copy);
+}

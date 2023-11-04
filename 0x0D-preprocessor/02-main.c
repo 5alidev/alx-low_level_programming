0c99697b5655d0d58161f159f666e75f322de333
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-04 07:53:06 -0700

    object like macro tasks

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#define SRC_FILE_NAME __BASE_FILE__
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", SRC_FILE_NAME);
+	return (0);
+}

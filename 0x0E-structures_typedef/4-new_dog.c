0455c8f4ecde485a3e7410daae097da2fe4a1a9a
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-06 15:43:41 -0800

    free owner and name

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,7 +21,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	doggy->name = strdup(name);
 	if (doggy->name == NULL)
 	{
-		free(doggy->name);
 		free(doggy);
 		return (NULL);
 	}
@@ -29,7 +28,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (doggy->owner == NULL)
 	{
 		free(doggy->name);
-		free(doggy->owner);
 		free(doggy);
 		return (NULL);
 	}

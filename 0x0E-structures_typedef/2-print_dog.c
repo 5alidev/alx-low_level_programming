88b25534af36682138b740e2e3c84fa04244389c
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-06 04:34:51 -0800

    make the code shorter using ternary operator

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,12 +12,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name == NULL)
-			printf("Name: %p\n", (void *)d->name);
-		if (d->owner == NULL)
-			printf("Owner: %p\n", (void *)d->owner);
-		printf("Name: %s\n", d->name);
+		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 	}
 }

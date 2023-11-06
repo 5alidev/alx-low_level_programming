e731650e97d76ea98d35325b5a61cd6c718fb58f
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-06 03:46:37 -0800

    create dog struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is the *Readme* file for the Alx **Structures-typedef** Tasks.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */

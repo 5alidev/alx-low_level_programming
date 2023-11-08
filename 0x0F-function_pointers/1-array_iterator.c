55a8a1320fdf70783a2cbb4a61ab98507d7ff0b6
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-08 08:52:22 -0800

    use size_t for i

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,12 +14,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	size_t i;
 
-	if (array == NULL || action == NULL)
-		return;
-	for (i = 0; i < size; i++)
+	if (array && size && action)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+			action(array[i]);
 	}
 }

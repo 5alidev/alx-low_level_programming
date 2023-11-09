e4be52e833a71d64329bd0bc149f06e4d49a2275
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-09 13:18:42 -0800

    add else

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		s = va_arg(args, char *);
 		if (s == NULL)
 			printf("nil");
-		printf("%s", s);
+		else
+			printf("%s", s);
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}

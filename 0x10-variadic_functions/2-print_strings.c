cc296b0607ec4c8b069c1e95c69a370f09ea23f4
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-09 13:22:25 -0800

    test if *sep is not 0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,9 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		s = va_arg(args, char *);
 		if (s == NULL)
 			printf("nil");
-		else
-			printf("%s", s);
-		if (separator != NULL && i < n - 1)
+		printf("%s", s);
+		if (separator != NULL && i < n - 1 && *separator != 0)
 			printf("%s", separator);
 	}
 	va_end(args);

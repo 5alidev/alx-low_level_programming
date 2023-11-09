f2e84cbfd3acfd4ec0ed6ec06ff5fc24a60136cb
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-09 13:48:46 -0800

    edit if condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (s == NULL)
 		{
 			printf("nil");
-			if (i != n)
+			if (i < n - 1)
 				printf("%s", separator);
 		}
 		else

e0fae7e7b83eeccab0865369caa3fa5d8a06244b
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-21 03:57:06 -0800

    print number of nodes

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef _lists_h_
+#define _lists_h_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * struct Node - singly linked list
+ * @n: data of type integer
+ * @next: pointer to next node
+ *
+ * Description: singly list node structure
+ */
+typedef struct Node
+{
+	int n;
+	struct Node *next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+#endif /* _lists_h_ */

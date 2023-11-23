a3effc6a101a4f883590a9cdd3d7b5ce1e6cae25
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-23 09:47:35 -0800

    convert binary to unsigned int

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+ALx **Bit - Manipulation** Tasks.
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef _main_h_
+#define _main_h_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+unsigned int binary_to_uint(const char *b);
+#endif /* _main_h */

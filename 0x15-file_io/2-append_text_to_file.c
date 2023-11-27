ee575b6193fc7423e75168a73b4c8114c6860d79
5alidev <khalidbenhamou.dev@outlook.com>
2023-11-27 15:14:50 -0800

    fix code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,15 +20,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		ssize_t write_status;
-
-		write_status = write(FD, text_content, strlen(text_content));
-		if (write_status == -1)
-		{
-			close(FD);
-			return (-1);
-		}
+		write(FD, text_content, strlen(text_content));
 	}
-	close(FD);
+	else
+	{
+		text_content = "";
+	}
 	return (1);
 }

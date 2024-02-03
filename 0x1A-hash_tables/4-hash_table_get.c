1e35ccc36fb1748014219c461f0e34441adbec07
5alidev <khalidbenhamou.dev@outlook.com>
2024-02-02 16:45:22 -0800

    edit function: check if either hash table or key are NULL

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *current = ht->array[index];
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
